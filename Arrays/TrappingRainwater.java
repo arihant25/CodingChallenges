1d60d10fe3bc6360bd6ca611c2bb1beb3209e3f2
Arihant Jain <arihant25jain@gmail.com>
2018-10-31 18:14:01 -0600

    quick sort

--- Arrays/TrappingRainwater.java
+++ Arrays/TrappingRainwater.java
@@ -1,2 +1,5 @@
 public class TrappingRainwater {
+    public static void main(String[] args) {
+
+    }
 }
---
+++ Searching/QuickSort.java
@@ -0,0 +1,39 @@
+public class QuickSort {
+    public static void main(String[] args) {
+        int array[] = new int[]{10, 7, 8, 9, 1, 5};
+        quickSort(array, 0, array.length - 1);
+        printArray(array);
+    }
+
+    private static void quickSort(int[] array, int low, int high) {
+        if (low < high) {
+            int pi = partition(array, low, high);
+            quickSort(array, low, pi - 1);
+            quickSort(array, pi + 1, high);
+        }
+    }
+
+    private static int partition(int[] array, int low, int high) {
+        int pivot = array[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (array[j] <= pivot) {
+                i++;
+                swap(array, i, j);
+            }
+        }
+        swap(array, i + 1, high);
+        return i + 1;
+    }
+
+    private static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    private static void printArray(int array[]) {
+        for (int i = 0; i < array.length; i++)
+            System.out.print(array[i] + " ");
+    }
+}

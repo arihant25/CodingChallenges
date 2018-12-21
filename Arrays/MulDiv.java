3899827d7b525ca2e332a8ad83bb404835300aaa
arihant25 <arihant25jain@gmail.com>
2018-12-20 23:56:15 -0700

    mul div

---
+++ Arrays/MulDiv.java
@@ -0,0 +1,8 @@
+/**
+ * Given an array of integers.
+ * To return a new array such that each element at index i of the new array is the product of all the numbers in the
+ * original array except the one at i.
+ */
+
+public class MulDiv {
+}

34372218d8e863fb1154c3647e08dc0d8b813235
Arihant Jain <arihant25jain@gmail.com>
2018-12-22 20:59:42 -0700

    to add sol with division

--- Arrays/MulDiv.java
+++ Arrays/MulDiv.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
  * Given an array of integers.
  * To return a new array such that each element at index i of the new array is the product of all the numbers in the
  * original array except the one at i.
+ * Time complexity: O(n)
  */
 
 public class MulDiv {
